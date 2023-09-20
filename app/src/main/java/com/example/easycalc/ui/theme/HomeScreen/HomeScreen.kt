bd0f3b58376d8aaedcc53891ed3d3c8eb227c856
theo_delbarre <theo_delbarre@ens.univ-artois.fr>
2023-09-20 15:49:51 +0200

    first commit

---
+++ app/src/main/java/com/example/easycalc/ui/theme/HomeScreen/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.easycalc.ui.theme.HomeScreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+
+}
