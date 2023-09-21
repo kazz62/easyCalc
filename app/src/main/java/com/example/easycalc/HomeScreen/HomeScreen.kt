c79e3d4637ef66c433dd3614e93d77bfab4ef36e
theo_delbarre <theo_delbarre@ens.univ-artois.fr>
2023-09-21 17:01:46 +0200

    feat/homepage Visual changes

--- app/src/main/java/com/example/easycalc/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/easycalc/HomeScreen/HomeScreen.kt
@@ -1,19 +1,33 @@
 package com.example.easycalc.HomeScreen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -28,72 +42,104 @@ fun HomeScreen(
     navController: NavController,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Row {
-            // Image en haut
-            Image(
-                painter = painterResource(id = R.drawable.ic_logo),
-                contentDescription = "Logo de l'application",
-                modifier = Modifier
-                    .size(64.dp)
-            )
-            // Titre
-            Text(
-                text = "Mental Calc",
-                style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
-            )
-        }
-
-        // Champ de texte avec image à gauche
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground), // Remplacez par votre image
-                contentDescription = "Image à gauche du champ de texte",
-                modifier = Modifier
-                    .size(24.dp)
-                    .padding(end = 8.dp)
-            )
-
-            // Champ de texte
-            BasicTextField(
-                value = "",
-                onValueChange = { /* Modifier la valeur du texte ici */ },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 8.dp)
-            )
-        }
-
-        // Bouton "Continuer" avec image à droite
-        Button(
-            onClick = { /* Action lorsque le bouton est cliqué */ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Continuer")
+            Spacer(modifier = Modifier.height(20.dp))
+            Row {
+                // Image en haut
                 Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground), // Remplacez par votre image
-                    contentDescription = "Image à droite du bouton",
+                    painter = painterResource(id = R.drawable.ic_logo),
+                    contentDescription = "Logo de l'application",
                     modifier = Modifier
-                        .size(24.dp)
-                        .padding(start = 8.dp)
+                        .size(64.dp)
+                        .alignByBaseline()
+                )
+                // Titre
+                Text(
+                    text = "Mental Calc",
+                    style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color.White ),
+                    modifier = Modifier.padding(16.dp)
                 )
             }
+
+            TextBar(
+                hint = "Nickname",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                // viewmodel
+            }
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Continue")
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "Image à droite du bouton",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = !it.isFocused
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
         }
     }
 }
