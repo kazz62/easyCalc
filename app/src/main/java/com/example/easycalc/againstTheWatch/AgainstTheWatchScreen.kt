dac71d581b5422788f9c55df1b59412cfad5bb5a
theo_delbarre <theo_delbarre@ens.univ-artois.fr>
2023-09-29 11:06:10 +0200

    feat/ag_the_watch wip

--- app/src/main/java/com/example/easycalc/againstTheWatch/AgainstTheWatchScreen.kt
+++ app/src/main/java/com/example/easycalc/againstTheWatch/AgainstTheWatchScreen.kt
@@ -1,6 +1,8 @@
 package com.example.easycalc.againstTheWatch
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -24,8 +28,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.easycalc.R
-import com.example.easycalc.menu.MenuButtons
-import com.example.easycalc.menu.MenuViewModel
 
 @Composable
 fun AgainstTheWatchScreen(
@@ -37,6 +39,22 @@ fun AgainstTheWatchScreen(
         color = MaterialTheme.colors.background,
         modifier = Modifier.fillMaxSize()
     ) {
+        val background = painterResource(id = R.drawable.quadrillage)
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+        }
+        Image(
+            painter = background,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        val imageHeight = with(LocalDensity.current) { 64.dp.toPx() }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth(),
@@ -44,16 +62,40 @@ fun AgainstTheWatchScreen(
         ) {
             Spacer(modifier = Modifier.height(80.dp))
 
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_ag_watch),
-                    contentDescription = "Logo of the application",
-                    modifier = Modifier
-                        .size(64.dp)
-                        .alignByBaseline()
+            SplitTitle("Against the", "Watch")
+
+            Spacer(modifier = Modifier.height(80.dp))
+
+        }
+    }
+}
+
+@Composable
+fun SplitTitle(titleLine1: String, titleLine2: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_ag_watch),
+                contentDescription = "Logo of the game",
+                modifier = Modifier
+                    .size(64.dp)
+                    .padding(16.dp)
+            )
+            Column {
+                Text(
+                    text = titleLine1,
+                    style = TextStyle(
+                        fontSize = 36.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    ),
+                    modifier = Modifier.padding(16.dp)
                 )
                 Text(
-                    text = "Against The Watch",
+                    text = titleLine2,
                     style = TextStyle(
                         fontSize = 36.sp,
                         fontWeight = FontWeight.Bold,
@@ -62,9 +104,6 @@ fun AgainstTheWatchScreen(
                     modifier = Modifier.padding(16.dp)
                 )
             }
-
-            Spacer(modifier = Modifier.height(80.dp))
-
         }
     }
 }
